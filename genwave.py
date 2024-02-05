a43436493364ab02fa7f947739057a1e77cf77af
Yifan Wang <yifan.wang@aei.mpg.de>
2024-02-05 22:16:15 +0100

    push to adapt the 2024.01 review version of SEOBNRv5EHM

--- genwave.py
+++ genwave.py
@@ -2,132 +2,52 @@ import numpy as np
 import lal
 from pycbc.types import TimeSeries
 from pycbc.waveform.utils import taper_timeseries
-from pyseobnr.generate_waveform import generate_modes_opt
-
-
-def Ylm(l: int, m: int, theta: float, phi: float) -> float:
-    """Get the spin-2 weighted spherical harmonics
-
-    Args:
-        l (int): ell
-        m (int): m
-        theta (float): inclination angle
-        phi (float): azimuthal angle
-
-    Returns:
-        float: The value of the Ylm
-    """
-    return lal.SpinWeightedSphericalHarmonic(theta, phi, -2, l, m)
-
-
-def combine_modes(iota: float, phi: float, modes_dict):
-    """Combine modes to compute the waveform polarizations in the direction
-    (iota,np.pi/2-phi)
-
-    Args:
-        iota (float): Inclination angle (rad)
-        phi (float): Azimuthal angle(rad)
-        modes_dict (Dict): Dictionary containing the modes, either time of frequency-domain
-
-    Returns:
-        np.array: Waveform in the given direction
-    """
-    sm = 0.0 #sm = h_+ - ih_x
-    for key in modes_dict.keys():
-        #print(key)
-        ell, m = [int(x) for x in key.split(",")]
-        sm += Ylm(ell, m, iota, np.pi / 2 - phi) * modes_dict[key]
-    return np.real(sm), -np.imag(sm)
-
-def timeMtoSec(timeM, M):
-    return timeM * M * lal.MTSUN_SI
-
-def HztoOmegainvM(Hz, M):
-    '''Convert GW frequency to Omega measured by unit of 1/M
-    '''
-    return np.pi * Hz * lal.MTSUN_SI * M
-
-def ampNRtoPhysicalTD(ampNR, M, distance):
-    return ampNR * (lal.C_SI * M *lal.MTSUN_SI)/distance
+from pyseobnr.generate_waveform import GenerateWaveform
 
 def genseob_td(**kwargs):
     '''PyCBC waveform generator for SEOBNRv5E
     '''
-    if kwargs['mass1'] < kwargs['mass2']:
-        #swap index 1 and 2 for mass and spinz
-        mass1 = kwargs['mass2']
-        mass2 = kwargs['mass1']
-        spin1z = kwargs['spin2z']
-        spin2z = kwargs['spin1z']
-        #no need to swap spinx and spiny cause they will be asserted to 0
-    else:
-        mass1 = kwargs['mass1']
-        mass2 = kwargs['mass2']
-        spin1z = kwargs['spin1z']
-        spin2z = kwargs['spin2z']
-
-    q = mass1 / mass2
-    M_tot = mass1 + mass2
-    chi_1 = [kwargs['spin1x'], kwargs['spin1y'], spin1z]
-    chi_2 = [kwargs['spin2x'], kwargs['spin2y'], spin2z]
+       
+    parameters = {"mass1": kwargs["mass1"],
+                  "mass2": kwargs["mass2"],
+                  "spin1x": kwargs["spin1x"],
+                  "spin1y": kwargs["spin1y"],
+                  "spin1z": kwargs["spin1z"],
+                  "spin2x": kwargs["spin2x"],
+                  "spin2y": kwargs["spin2y"],
+                  "spin2z": kwargs['spin2z'],
     
-    eccentricity = kwargs['eccentricity']
-    rel_anomaly = kwargs['rel_anomaly']
-    
-    delta_t = kwargs['delta_t']
-    distance = 1e6 * lal.PC_SI * kwargs['distance']
-    iota = kwargs['inclination']
-    phi_angle = kwargs['coa_phase']
-    #initial orbital angular frequency in unit of inverse M
-    omega0 = HztoOmegainvM(kwargs['f_lower'], M_tot)
+                  "distance": kwargs['distance'],
+                  "inclination": kwargs["inclination"],
+                
+                  "phi_ref": kwargs["coa_phase"],
+                  "f22_start": kwargs["f_lower"],
+                  #"f_ref": kwargs["f_ref"],
+                  "deltaT": kwargs['delta_t'],
+                  "ModeArray": [(2,2)],
+                             #[(2,2), (2,1), (3,2), (3,3), (4,3), (4,4), (5,5)]
+                             # Specify which modes are to be returned
 
-    #sanity checks
-    assert chi_1[0] == 0 and chi_1[1] == 0 and chi_2[0] == 0 and chi_2[1] == 0, \
-                    "Invalid spin! SEOBNRv5E only supports aligned spin!"
-    assert eccentricity >=0 and eccentricity <=1, \
-                    "Invalid eccentricity! SEOBNRv5E only supports eccentricity in [0,1]"
-    
-    omega_start = omega0
-    return_modes = [(2,2)]
-    settings = {
-        'M':M_tot,
-        'dt':delta_t,
-        "mode_factorization":4, 
-        "x_prescription":"xAvg", 
-        'return_modes':return_modes,
-        'EccIC':0, #then omega0 corresponds to the initial instantaneous angular frequency. 
-    }
-
-    # SEOBNRv5EHM waveform evaluation
-    time, hlm = generate_modes_opt(q, chi_1[2], chi_2[2], 
-        omega_start, omega0, #Omega_start, Omega_ref
-        eccentricity, rel_anomaly, 
-        approximant="SEOBNRv5EHM", RRForce='Ecc', settings=settings)
-
-    # Compute the negative m modes (aligned-spin)
-    hlm_new = {}
-    for key in hlm:
-        ell, m = key.split(',')
-        hlm_new[ell + ',-' + m] = (-1)**int(ell) * np.conj(hlm[key])
-
-    hlm = hlm | hlm_new
-
-    # Get the plus and cross polarizations (in geometric units) by adding all the modes
-    hp_NR, hc_NR = combine_modes(iota, phi_angle, hlm)
-
-    # Convert to SI units
-    t_s = timeMtoSec(time, M_tot)
-    hp = ampNRtoPhysicalTD(hp_NR, M_tot, distance)
-    hc = ampNRtoPhysicalTD(hc_NR, M_tot, distance)
+                  "rel_anomaly": kwargs['rel_anomaly'],
+                  "eccentricity": kwargs['eccentricity'],
+                  "approximant": "SEOBNRv5EHM",
+                  
+                  "postadiabatic": False,
+                  "RRFroce": "Ecc",
+                  "EccIC": 1, # EccIC = 0 for instantaneous initial orbital frequency
+                             # EccIC = 1 for orbit-averaged initial orbital frequency
+                  }
+    waveform = GenerateWaveform(parameters)
+    h_plus, h_cross = waveform.generate_td_polarizations()
 
     # Build the TimeSeries format
-    hpt = TimeSeries(hp, delta_t=delta_t, epoch=t_s[0])
-    hct = TimeSeries(hc, delta_t=delta_t, epoch=t_s[0])
+    hp = TimeSeries(h_plus.data.data, delta_t=h_plus.deltaT, epoch=h_plus.epoch)
+    hc = TimeSeries(h_cross.data.data, delta_t=h_cross.deltaT, epoch=h_cross.epoch)
 
-    return hpt,hct
+    return hp,hc
 
 def genseob_fd(**kwargs):
-    kwargs['delta_t'] = 1.0 / 2048
+    kwargs['delta_t'] = 1.0 / kwargs['f_final'] / 2
     hp, hc = genseob_td(**kwargs)
 
     # Resize to the right duration

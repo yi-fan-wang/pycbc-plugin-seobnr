7423e1a47825cb9a5170cf0716f6e6f6d6f7ead9
Yifan Wang <yifan.wang@aei.mpg.de>
2024-02-08 00:51:42 +0100

    typo

--- genwave.py
+++ genwave.py
@@ -3,12 +3,12 @@ from pycbc.types import TimeSeries
 from pycbc.waveform.utils import taper_timeseries
 from pyseobnr.generate_waveform import GenerateWaveform
 
-def gen_seobnrv5ehm_td(**par):
+def gen_seobnrv5ehm_td(**p):
     '''PyCBC waveform generator for SEOBNRv5E
 
     Parameters
     ----------
-    dict: par
+    dict: p
         A dictionary of parameters used by pycbc.waveform.get_td_waveform
 
     Returns
@@ -23,7 +23,7 @@ def gen_seobnrv5ehm_td(**par):
     # Some waveform input parameters from PyCBC have the same naming conventions
     # as PySEOBNR, thus they can be directly used. We only update the settings 
     # used specifically by PySEOBNR
-    par.update({
+    p.update({
         "approximant": "SEOBNRv5EHM", # I call it "SEOBNRv5E" in PyCBC 
         "ModeArray": [(2,2)],      # only consider (2,2) mode
         "rel_anomaly": p["rel_anomaly"], # relativity anomaly,  needed for eccentric waveform
@@ -31,12 +31,12 @@ def gen_seobnrv5ehm_td(**par):
         "f22_start": p["f_lower"], # starting frequency
         "f_ref": p["f_lower"],     # reference frequency
         "deltaT": p["delta_t"],    
-        "postadiabatic": False     # turn off postadiabatic correction
+        "postadiabatic": False ,   # turn off postadiabatic correction
         "h_0": 1.0,                # initial time step in the integration of the ODEs.
         "lmax_nyquist": 1          # maximum L to be checked against Nyquist frequency
         })
 
-    waveform = GenerateWaveform(par)
+    waveform = GenerateWaveform(p)
     hp, hc = waveform.generate_td_polarizations()
 
     # Build the PyCBC TimeSeries format

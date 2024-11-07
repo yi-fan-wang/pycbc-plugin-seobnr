117a2fb742783730b6acbf6d58974ae0263e51a3
Yifan Wang <yifan.wang@aei.mpg.de>
2024-11-07 12:04:08 +0100

    add seobnrv5ehm

--- genwave.py
+++ genwave.py
@@ -4,25 +4,6 @@ from pycbc.types import TimeSeries, FrequencySeries
 from pyseobnr.generate_waveform import GenerateWaveform
 
 def gen_seobnrv5e_td(**p):
-    '''PyCBC waveform generator for SEOBNRv5E
-
-    Parameters
-    ----------
-    dict: p
-        A dictionary of parameters used by pycbc.waveform.get_td_waveform
-
-    Returns
-    -------
-    hp: Array
-        The plus polarization of the waveform in time domain
-    hc: Array
-        The cross polarization of the waveform in time domain
-    """
-    '''
-
-    # Some waveform input parameters from PyCBC have the same naming 
-    # conventions as PySEOBNR, thus they can be directly inherited. 
-    # We only update the settings used uniquely by PySEOBNR
     p.update({
         "approximant": "SEOBNRv5EHM", # I call it "SEOBNRv5E" in PyCBC 
         "ModeArray": [(2,2)],      # only consider (2,2) mode
@@ -48,29 +29,10 @@ def gen_seobnrv5e_td(**p):
     return hp,hc
 
 def gen_seobnrv5e_fd(**p):
-    '''PyCBC waveform generator for SEOBNRv5E
-
-    Parameters
-    ----------
-    dict: p
-        A dictionary of parameters used by pycbc.waveform.get_td_waveform
-
-    Returns
-    -------
-    hp: Array
-        The plus polarization of the waveform in frequency domain
-    hc: Array
-        The cross polarization of the waveform in frequency domain
-    """
-    '''
-
-    # Some waveform input parameters from PyCBC have the same naming 
-    # conventions as PySEOBNR, thus they can be directly inherited. 
-    # We only update the settings used uniquely by PySEOBNR
     p.update({
-        "approximant": "SEOBNRv5EHM", # I call it "SEOBNRv5E" in PyCBC 
+        "approximant": "SEOBNRv5EHM",
         "ModeArray": [(2,2)],      # only consider (2,2) mode
-        "rel_anomaly": p["rel_anomaly"], # relativity anomaly,  needed for eccentric waveform
+        "rel_anomaly": p["rel_anomaly"] if "rel_anomaly" in p else 0, # relativity anomaly,  needed for eccentric waveform
         "phi_ref": p["coa_phase"], # reference phase needed by SEOBNRv5
         "f22_start": p["f_lower"], # starting frequency
         "f_ref": p["f_lower"],     # reference frequency
@@ -94,41 +56,27 @@ def gen_seobnrv5e_fd(**p):
 
     return hp_pycbc,hc_pycbc
 
-def gen_seobnrv5phm_td(**p):
-    '''PyCBC waveform generator for SEOBNRv5HM
-
-    Parameters
-    ----------
-    dict: p
-        A dictionary of parameters used by pycbc.waveform.get_td_waveform
-
-    Returns
-    -------
-    hp: Array
-        The plus polarization of the waveform in time domain
-    hc: Array
-        The cross polarization of the waveform in time domain
-    """
-    '''
-
-    # Some waveform input parameters from PyCBC have the same naming 
-    # conventions as PySEOBNR, thus they can be directly inherited. 
-    # We only update the settings used uniquely by PySEOBNR
+def gen_seobnrv5ehm_fd(**p):
     p.update({
-        "approximant": "SEOBNRv5PHM",  
+        "approximant": "SEOBNRv5EHM",
+        "rel_anomaly": p["rel_anomaly"] if "rel_anomaly" in p else 0, # relativity anomaly,  needed for eccentric waveform
         "phi_ref": p["coa_phase"], # reference phase needed by SEOBNRv5
         "f22_start": p["f_lower"], # starting frequency
-        "deltaT": p["delta_t"],    
+        "f_ref": p["f_lower"],     # reference frequency
+        "deltaF": p["delta_f"],    
         })
 
     waveform = GenerateWaveform(p)
-    hp, hc = waveform.generate_td_polarizations()
+    hp, hc, template_duration = waveform.generate_fd_polarizations()
 
     # Build the PyCBC TimeSeries format
-    hp = TimeSeries(hp.data.data[:], delta_t=hp.deltaT, epoch=hp.epoch)
-    hc = TimeSeries(hc.data.data[:], delta_t=hc.deltaT, epoch=hc.epoch)
+    hp_pycbc = FrequencySeries(hp.data.data[:], delta_f=hp.deltaF, epoch=hp.epoch)
+    hc_pycbc = FrequencySeries(hc.data.data[:], delta_f=hc.deltaF, epoch=hp.epoch)
+    
+    hp_pycbc.eob_template_duration = template_duration
+    hc_pycbc.eob_template_duration = template_duration
 
-    return hp,hc
+    return hp_pycbc,hc_pycbc
 
 def seobnrv5phm_length_in_time(**kwds):
     from pycbc.waveform.waveform import get_hm_length_in_time

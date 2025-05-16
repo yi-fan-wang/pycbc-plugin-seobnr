213d9d26495fc86450b77b5e3091829724c1c787
Yifan Wang <yifan.wang@aei.mpg.de>
2025-05-16 10:18:51 +0200

    add higher mode

--- genwave.py
+++ genwave.py
@@ -1,96 +1,74 @@
 import numpy as np
 from pycbc.types import TimeSeries, FrequencySeries
-#from pycbc.waveform.utils import taper_timeseries
 from pyseobnr.generate_waveform import GenerateWaveform
 
-def gen_seobnrv5e_td(**p):
-    p.update({
-        "approximant": "SEOBNRv5EHM", # I call it "SEOBNRv5E" in PyCBC 
-        "ModeArray": [(2,2)],      # only consider (2,2) mode
-        "rel_anomaly": p["rel_anomaly"], # relativity anomaly,  needed for eccentric waveform
-        "phi_ref": p["coa_phase"], # reference phase needed by SEOBNRv5
-        "f22_start": p["f_lower"], # starting frequency
-        "f_ref": p["f_lower"],     # reference frequency
-        "deltaT": p["delta_t"],    
-        #"postadiabatic": False ,   # turn off postadiabatic correction, 
-                                   # default is False in SEOBNRv5EHM
-        #"h_0": 1.0,                # initial time step in the integration of the ODEs. 
-                                    # Default Value is 1.0
-        "lmax_nyquist": 1          # maximum L to be checked against Nyquist frequency
-        })
-
-    waveform = GenerateWaveform(p)
-    hp, hc = waveform.generate_td_polarizations_conditioned_1()
-
-    # Build the PyCBC TimeSeries format
-    hp = TimeSeries(hp.data.data[:], delta_t=hp.deltaT, epoch=hp.epoch)
-    hc = TimeSeries(hc.data.data[:], delta_t=hc.deltaT, epoch=hc.epoch)
-
-    return hp,hc
-
-def gen_seobnrv5e_fd(**par_pycbc):
+def convert_pycbc_to_seobnr(domain, p_input):
     '''
-    Convert PyCBC waveform parameters to PySEOB waveform parameters and generate the waveform in frequency domain.
-    Parameters are hardcoded to only use SEOBNRv5EHM 's (2,2) mode and not check Nyquist frequency
+    Convert PyCBC waveform parameters to pyseobnr conventions.
     '''
-    assert par_pycbc["approximant"] == "SEOBNRv5E", "Approximant must be SEOBNRv5E"
-    
-    pconvert = {
-        "approximant": "SEOBNRv5EHM", 
-        "ModeArray": [(2,2)],
-        "lmax_nyquist": 1,                  # maximum L to be checked against Nyquist frequency
-        "rel_anomaly": par_pycbc["rel_anomaly"] if "rel_anomaly" in par_pycbc else 0,
-                                            # relativity anomaly, needed for eccentric waveform, default: 0
-        "phi_ref":   par_pycbc["coa_phase"],# reference phase needed by SEOBNRv5
-        "f22_start": par_pycbc["f_lower"],  # starting frequency
-        "f_ref":  par_pycbc["f_lower"],     # reference frequency
-        "deltaF": par_pycbc["delta_f"],        
-        }
+    p = p_input.copy()
+    p["rel_anomaly"] = p.pop("rel_anomaly", 0) # relativity anomaly, needed for eccentric waveform
+    p["phi_ref"] = p.pop("coa_phase") # reference phase needed by SEOBNRv5
+    p["f22_start"] = p.pop("f_lower") # starting frequency
+    p["f_ref"] = p["f22_start"]     # reference frequency, use starting frequency at the moment
+    if domain == "frequency":
+        p["deltaF"] = p.pop("delta_f")  # frequency spacing
+    elif domain == 'time':
+        p["deltaT"] = p.pop("delta_t")  # time spacing
+    else:
+        raise ValueError("domain must be 'frequency' or 'time'")
+    return p
 
-    # ignoring the debugging parameters at the moment
-    par_debug = {"postadiabatic": False ,   # turn off postadiabatic correction, default is False in SEOBNRv5EHM
-                 "h_0": 1.0,  # initial time step in the integration of the ODEs. Default Value is 1.0
-                 }
+def base_seobnrv5e(highermode, domain, p_pycbc):
+    '''
+    Convert PyCBC waveform parameters to pyseobnr waveform parameters and 
+    generate the waveform in frequency domain. Parameters are hardcoded to 
+    only use SEOBNRv5EHM 's (2,2) mode and not check Nyquist frequency
+    '''    
+    p_eob = convert_pycbc_to_seobnr(domain, p_pycbc)
+    p_eob["approximant"] = "SEOBNRv5EHM"
+    if not highermode:
+        p_eob["ModeArray"] = [(2,2)]
+    p_eob["lmax_nyquist"] = 1
+    waveform = GenerateWaveform(p_eob)
 
-    par_pyseobnr = par_pycbc.copy()
-    par_pyseobnr.update(pconvert)
+    if domain == "frequency":
+        # generate the frequency domain waveform
+        hp, hc, template_duration = waveform.generate_fd_polarizations()
 
-    waveform = GenerateWaveform(par_pyseobnr) # not **par_pyseobnr because the arguments is a dictionary not **kwargs
-    hp, hc, template_duration = waveform.generate_fd_polarizations()
+        # Build the PyCBC TimeSeries format
+        hp_pycbc = FrequencySeries(hp.data.data[:], delta_f = hp.deltaF, epoch = hp.epoch)
+        hc_pycbc = FrequencySeries(hc.data.data[:], delta_f = hc.deltaF, epoch = hp.epoch)
+        
+        # Fix the pyseobnr convention to let peak time be at 0
+        hp_pycbc = hp_pycbc.cyclic_time_shift(hp_pycbc.start_time + hp_pycbc.duration)
+        hc_pycbc = hc_pycbc.cyclic_time_shift(hc_pycbc.start_time + hc_pycbc.duration)
 
-    # Build the PyCBC TimeSeries format
-    hp_pycbc = FrequencySeries(hp.data.data[:], delta_f=hp.deltaF, epoch=hp.epoch)
-    hc_pycbc = FrequencySeries(hc.data.data[:], delta_f=hc.deltaF, epoch=hp.epoch)
-    
-    hp_pycbc = hp_pycbc.cyclic_time_shift(hp_pycbc.start_time+hp_pycbc.duration)
-    hc_pycbc = hc_pycbc.cyclic_time_shift(hc_pycbc.start_time+hc_pycbc.duration)
+        hp_pycbc.eob_template_duration = template_duration
+        hc_pycbc.eob_template_duration = template_duration
+    elif domain == "time":
+        # generate the time domain waveform with the start tapered
+        hp, hc = waveform.generate_td_polarizations_conditioned_1()
 
-    hp_pycbc.eob_template_duration = template_duration
-    hc_pycbc.eob_template_duration = template_duration
+        # Build the PyCBC TimeSeries format
+        hp_pycbc = TimeSeries(hp.data.data[:], delta_t = hp.deltaT, epoch = hp.epoch)
+        hc_pycbc = TimeSeries(hc.data.data[:], delta_t = hc.deltaT, epoch = hc.epoch)
+    else:
+        raise ValueError("domain must be 'frequency' or 'time'")
 
     return hp_pycbc,hc_pycbc
+
+def gen_seobnrv5e_td(**p):
+    return base_seobnrv5e(False, "time", p)
+
+def gen_seobnrv5e_fd(**p):
+    return base_seobnrv5e(False, "frequency", p)
+
+def gen_seobnrv5ehm_td(**p):
+    return base_seobnrv5e(True, "time", p)
 
 def gen_seobnrv5ehm_fd(**p):
-    p.update({
-        "approximant": "SEOBNRv5EHM",
-        "rel_anomaly": p["rel_anomaly"] if "rel_anomaly" in p else 0, # relativity anomaly,  needed for eccentric waveform
-        "phi_ref": p["coa_phase"], # reference phase needed by SEOBNRv5
-        "f22_start": p["f_lower"], # starting frequency
-        "f_ref": p["f_lower"],     # reference frequency
-        "deltaF": p["delta_f"],    
-        })
-
-    waveform = GenerateWaveform(p)
-    hp, hc, template_duration = waveform.generate_fd_polarizations()
-
-    # Build the PyCBC TimeSeries format
-    hp_pycbc = FrequencySeries(hp.data.data[:], delta_f=hp.deltaF, epoch=hp.epoch)
-    hc_pycbc = FrequencySeries(hc.data.data[:], delta_f=hc.deltaF, epoch=hp.epoch)
-    
-    hp_pycbc.eob_template_duration = template_duration
-    hc_pycbc.eob_template_duration = template_duration
-
-    return hp_pycbc,hc_pycbc
+    return base_seobnrv5e(True, "frequency", p)
 
 def seobnrv5phm_length_in_time(**kwds):
     from pycbc.waveform.waveform import get_hm_length_in_time
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup.py file to hook up pyseobnr with pycbc waveform plugin
 
 from setuptools import Extension, setup, Command, find_packages
 
-VERSION = '0.0'
+VERSION = '0.1'
 
 setup (
     name = 'pycbc-seobnr',
@@ -13,13 +13,13 @@ setup (
     author = 'Yifan Wang',
     author_email = 'yifan.wang@aei.mpg.de',
     url = 'https://github.com/yi-fan-wang/pycbc-plugin-seobnr',
-    #download_url = 'https://github.com/gwastro/revchirp/tarball/v%s' % VERSION,
     keywords = ['effective one body', 'gravitational waves', 'pycbc'],
     packages = find_packages(),
     py_modules = ['genwave'],
     #package_dir = {'':'src'},
     #package_dir={'PyTGR': 'src'},
-    entry_points = {"pycbc.waveform.td":["SEOBNRv5E_tdtaper = genwave:gen_seobnrv5e_td"],
+    entry_points = {"pycbc.waveform.td":["SEOBNRv5E_tdtaper = genwave:gen_seobnrv5e_td",
+                                         "SEOBNRv5EHM_tdtaper = genwave:gen_seobnrv5ehm_td"],
                     "pycbc.waveform.fd":["SEOBNRv5E = genwave:gen_seobnrv5e_fd",
                                          "SEOBNRv5EHM = genwave:gen_seobnrv5ehm_fd"],
                     "pycbc.waveform.length":["SEOBNRv5PHM = genwave:seobnrv5phm_length_in_time"]
@@ -31,6 +31,7 @@ setup (
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
         'Intended Audience :: Science/Research',
         'Natural Language :: English',
         'Topic :: Scientific/Engineering',
